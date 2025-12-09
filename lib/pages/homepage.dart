d55885e2647da8bf2c75af8db83bb507d3d41b09
Syed Zain-ul-Abideen <105231652+ZaynShah231@users.noreply.github.com>
2025-12-09 17:59:58 +0500

    Add files via upload

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:live_location/helper/gps.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final GPS _gps = GPS();
+  Position? _userPosition;
+  Exception? _exception;
+
+  void _handlePositionStream(Position position){
+    setState(() {
+      _userPosition = position;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    Widget child;
+    if(_exception !=null){
+      child = Text('Please provide GPS Permissions');
+    }else if(_userPosition == null){
+      child = CircularProgressIndicator();
+    } else{
+      child = Text(_userPosition.toString());
+    }
+    return Scaffold(
+      body: Center(child: child),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _gps.startPositionStream(_handlePositionStream).catchError((e){
+      setState(() {
+        _exception = e;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _gps.stopPositionStream();
+  }
+}
